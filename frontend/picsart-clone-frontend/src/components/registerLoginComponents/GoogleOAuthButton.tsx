2f9f6785d91ac904be310979a4d9b40a80aeedda
Aram47 <aram.minasyan.00@gmail.com>
2025-04-14 16:06:52 +0400

    [FEAT] google OAuth

--- frontend/picsart-clone-frontend/src/components/registerLoginComponents/GoogleOAuthButton.tsx
+++ frontend/picsart-clone-frontend/src/components/registerLoginComponents/GoogleOAuthButton.tsx
@@ -1,6 +1,8 @@
-import { GoogleLogin, CredentialResponse } from '@react-oauth/google';
+import { GoogleOAuthProvider, GoogleLogin, CredentialResponse } from '@react-oauth/google';
 
 const OAuthButton = () => {
+  const clientId = '782626810761-bbn40sec9a00nuktu5c52n3mpon7emb9.apps.googleusercontent.com';
+
   const handleSuccess = (credentialResponse: CredentialResponse) => {
     if (credentialResponse.credential) {
       console.log("ID Token:", credentialResponse.credential);
@@ -16,7 +18,11 @@ const OAuthButton = () => {
   return (
     <div className="flex flex-col items-center my-6">
       <p className="text-sm text-gray-500 mb-2">Или войдите через Google</p>
-      <GoogleLogin onSuccess={handleSuccess} onError={handleError} />
+      <GoogleOAuthProvider clientId={clientId}>
+        <div>
+          <GoogleLogin onSuccess={handleSuccess} onError={handleError} />
+        </div>
+      </GoogleOAuthProvider>
     </div>
   );
 };
